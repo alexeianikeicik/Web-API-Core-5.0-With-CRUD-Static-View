2d9d75336a6ba085d6882ad994ea7584eb3786ef
Алексей <anikeichik@tut.by>
2021-07-19 22:07:38 +0300

    Добавьте файлы проекта.

---
+++ WebAPICore5_0W/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebAPICore5_0W
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
